27126ee27582fa4feace7c346040fa98959cbf34
Paramintr Tienpasertkij <82603421+ptienpasertkij@users.noreply.github.com>
2024-07-31 11:15:18 -0400

    created test file

---
+++ testvlc.py
@@ -0,0 +1,12 @@
+import vlc
+
+def play_audio(file):
+    player = vlc.MediaPlayer(file)
+    player.play()
+    time.sleep(5)  # Wait for the player to start
+    while player.is_playing():
+        time.sleep(1)
+    player.stop()
+
+if __name__ == "__main__":
+    play("/home/autoannc/Music/5mintest.wav")
