7f49e3fda78edc83249091ab8ce406b601b4e07f
Andrew Slaugh <aslaugh@most.org>
2024-08-30 11:09:08 -0400

    updating recording files

--- schedule_library.py
+++ schedule_library.py
@@ -4,7 +4,7 @@ import time
 def print_hello():
     print("Hello World")
     
-schedule.every().monday.at("time").do(print_hello)
+schedule.every(5).seconds.do(print_hello)
 while True:
     schedule.run_pending()
     time.sleep(1)
